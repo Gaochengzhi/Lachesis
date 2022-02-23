345db101313119e21aa491e601242f5438037ed6
Gaochengzhi <2673730435@qq.com>
2022-02-23 22:20:35 +0800

    debug_mode and refactory

--- build_Lachesis/src/headline.c
+++ build_Lachesis/src/headline.c
@@ -3,10 +3,11 @@
  * License           : The MIT License (MIT)
  * Author            : Gao Chengzhi <2673730435@qq.com>
  * Date              : 16.02.2022
- * Last Modified Date: 18.02.2022
+ * Last Modified Date: 23.02.2022
  * Last Modified By  : Gao Chengzhi <2673730435@qq.com>
  */
 #include "./headline.h"
+#include "lachesis_debug.h"
 #include <stdio.h>
 #define _COSTOMIZE "default"
 void lec_print_headline()
@@ -21,5 +22,13 @@ void lec_print_headline()
     printf("Version: %s, ", uname_pointer.release);
     printf("Machine: %s. ", uname_pointer.machine);
     puts("\n");
-    puts("NOTE: this version is made to implement the lval type");
+    puts("NOTE: This version is for creating debug mode and code refactory!");
+    puts("      Type -g or --g to debug");
+
+    if (_debug_mode == 1) {
+        char* types = "\nNote:\nLVAL_NUM=0,\
+            \nLVAL_ERR=1,\nLVAL_SYMBOL=2,\
+            \nLVAL_SEXPR=3\n";
+        puts(types);
+    }
 }
